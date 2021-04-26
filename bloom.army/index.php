f0464098c44f879dd255561b6fb2fb6793106d21
Lars <larssieboy18@users.noreply.github.com>
2021-04-26 22:14:22 +0200

    Add files via upload

---
+++ bloom.army/index.php
@@ -0,0 +1,51 @@
+<head>
+<title>Abby Army</title>
+
+    <meta content="Bloom Army" property="og:title">
+  <meta content="Bloom Army Stronk" property="og:description">
+    <meta content="https://bloom.army" property="og:url">
+    <meta content="https://bloom.army/abby.png" property="og:image">
+    <meta content="#FC5C00" data-react-helmet="true" name="theme-color">
+</head>
+
+
+
+
+
+<style>
+    body {
+  background-image: url("abby.png");
+  background-repeat: repeat;
+  height: 100000px;
+}
+#hidden_coupon {
+    display:none;
+}
+dontopenme {
+    display:none;
+}
+</style>
+
+<body>
+    
+    <div id="hidden_coupon">
+        Click me for coupon code!
+    </div>
+    
+    
+<script>
+    function couponCode() {
+        couponE = getElementByID("hidden_coupon");
+        couponE.onclick(
+            window.print('AllStaffRDucks')
+    )
+    }
+</script>
+</body>
+
+<dontopenme>
+ THE SECRET HAS ALREADY BEEN FOUND
+
+    If you see this message, screenshot it in #general on Discord and tag Eagle. I will gift you with $10 account credit :P
+    P.S. The coupon code above is a ruse, don't believe it, or I mean, could this be a tactic to discourage you from trying and possibly getting 50% off? Who knows, but will you try?
+</dontopenme>
