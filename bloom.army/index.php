c929e6efecd2345a5577877327261d9df88a884b
Razboy20 <razboy20@gmail.com>
2021-04-26 15:32:06 -0500

    add infinite scrolling

--- bloom.army/index.php
+++ bloom.army/index.php
@@ -1,51 +1,59 @@
-<head>
-<title>Abby Army</title>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <title>Abby Army</title>
 
-    <meta content="Bloom Army" property="og:title">
-  <meta content="Bloom Army Stronk" property="og:description">
-    <meta content="https://bloom.army" property="og:url">
-    <meta content="https://bloom.army/abby.png" property="og:image">
-    <meta content="#FC5C00" data-react-helmet="true" name="theme-color">
-</head>
+        <meta content="Bloom Army" property="og:title" />
+        <meta content="Bloom Army Stronk" property="og:description" />
+        <meta content="https://bloom.army" property="og:url" />
+        <meta content="https://bloom.army/abby.png" property="og:image" />
+        <meta content="#FC5C00" data-react-helmet="true" name="theme-color" />
+        <style>
+            body {
+                background-image: url("https://bloom.army/abby.png");
+                background-repeat: repeat;
+                height: 3000px;
+            }
+            #hidden_coupon {
+                display: none;
+            }
+            dontopenme {
+                display: none;
+            }
+        </style>
 
+        <script src="https://unpkg.com/darkreader@4.9.32/darkreader.js"></script>
+    </head>
 
+    <body>
+        <div id="hidden_coupon">Click me for coupon code!</div>
 
+        <script>
+            DarkReader.disable();
 
+            const body = document.querySelector("body");
+            let height = screen.height * 3;
+            body.style.height = height;
 
-<style>
-    body {
-  background-image: url("abby.png");
-  background-repeat: repeat;
-  height: 100000px;
-}
-#hidden_coupon {
-    display:none;
-}
-dontopenme {
-    display:none;
-}
-</style>
+            window.addEventListener("scroll", () => {
+                if (window.scrollY + screen.height > height - screen.height) {
+                    height += screen.height * 3;
+                    body.style.height = height;
+                }
+            });
 
-<body>
-    
-    <div id="hidden_coupon">
-        Click me for coupon code!
-    </div>
-    
-    
-<script>
-    function couponCode() {
-        couponE = getElementByID("hidden_coupon");
-        couponE.onclick(
-            window.print('AllStaffRDucks')
-    )
-    }
-</script>
-</body>
+            function couponCode() {
+                couponE = getElementByID("hidden_coupon");
+                couponE.onclick(window.print("AllStaffRDucks"));
+            }
+        </script>
 
-<dontopenme>
- THE SECRET HAS ALREADY BEEN FOUND
-
-    If you see this message, screenshot it in #general on Discord and tag Eagle. I will gift you with $10 account credit :P
-    P.S. The coupon code above is a ruse, don't believe it, or I mean, could this be a tactic to discourage you from trying and possibly getting 50% off? Who knows, but will you try?
-</dontopenme>
+        <dontopenme>
+            THE SECRET HAS ALREADY BEEN FOUND 
+            
+            If you see this message, screenshot it in #general on Discord and tag Eagle. I will gift you with $10
+            account credit :P P.S. The coupon code above is a ruse, don't believe it, or I mean, could this be a tactic to discourage you from trying
+            and possibly getting 50% off? Who knows, but will you try?
+        </dontopenme>
+    </body>
+</html>
